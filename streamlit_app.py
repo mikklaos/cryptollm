d83dabea5cce1b8d1eb62fc8a47523a84a5a4cd7
Mikk <mikk.laos@gmail.com>
2024-06-03 09:40:17 +0300

    sql3-binary fix remove redundant code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 from crewai import Crew, Process
 from mainCrew import CryptoAgents
 from cryptoTasks import CryptoTasks
